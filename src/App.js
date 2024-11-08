500ad2c47f56e1f431bcf3e4435209eace521563
root <root@ip-172-31-16-230.us-east-2.compute.internal>
2024-11-08 00:42:54 +0000

    My React Project

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Name: Salman Mirza <br/>
+	  Emp ID: 20096
         </p>
         <a
           className="App-link"
